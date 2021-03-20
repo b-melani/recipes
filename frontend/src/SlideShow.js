73385dd045cec768ada3a0d8d406b1911872edea
Meli <bodzamelani@gmail.com>
2021-03-20 22:03:16 +0100

    deleted comments

--- frontend/src/SlideShow.js
+++ frontend/src/SlideShow.js
@@ -24,36 +24,33 @@ export default function SlideShow() {
   }
 
   return (
-    <div className="w-60 text-center">
-      <Carousel
-        className="w-60"
-        swipeable={false}
-        draggable={false}
-        showDots={true}
-        responsive={responsive}
-        ssr={true} // means to render carousel on server-side.
-        infinite={true}
-        autoPlay={true}
-        autoPlaySpeed={1500}
-        keyBoardControl={true}
-        //customTransition="all .5"
-        transitionDuration={500}
-        containerClass="carousel-container"
-        removeArrowOnDeviceType={['tablet', 'mobile']}
-        //deviceType={this.props.deviceType}
-        dotListClass="custom-dot-list-style"
-        itemClass="carousel-item-padding-40-px"
-      >
-        <div>
-          <img src={donuts} width="50%" alt="" />
-        </div>
-        <div>
-          <img src={rice} width="40%" alt="" />
-        </div>
-        <div>
-          <img src={cake} width="60%" alt="" />
-        </div>
-      </Carousel>
-    </div>
+    <Carousel
+      swipeable={false}
+      draggable={false}
+      showDots={true}
+      responsive={responsive}
+      ssr={true} // means to render carousel on server-side.
+      infinite={true}
+      autoPlay={true}
+      autoPlaySpeed={1500}
+      keyBoardControl={true}
+      //customTransition="all .5"
+      transitionDuration={500}
+      containerClass="carousel-container"
+      removeArrowOnDeviceType={['tablet', 'mobile']}
+      //deviceType={this.props.deviceType}
+      dotListClass="custom-dot-list-style"
+      itemClass="carousel-item-padding-40-px"
+    >
+      <div>
+        <img src={donuts} width="50%" alt="" />
+      </div>
+      <div>
+        <img src={rice} width="40%" alt="" />
+      </div>
+      <div>
+        <img src={cake} width="60%" alt="" />
+      </div>
+    </Carousel>
   )
 }
