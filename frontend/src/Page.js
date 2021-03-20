28d770913ca50890567d2f530bba410008f61c48
Meli <bodzamelani@gmail.com>
2021-03-20 21:46:55 +0100

    Added SlideShow to Page

--- frontend/src/Page.js
+++ frontend/src/Page.js
@@ -3,12 +3,16 @@ import { BrowserRouter as Router } from 'react-router-dom'
 import Routing from './Routing'
 import Menu from './Menu'
 import Header from './Header'
+import SlideShow from './SlideShow'
 
 export default function Page() {
   return (
     <Router>
       <header>
         <Header />
+        <div>
+          <SlideShow />
+        </div>
         <div className="col-12">
           <Menu />
         </div>
