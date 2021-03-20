05ae79a25df18665760a0ba9e20a5b12bc90ee77
Meli <bodzamelani@gmail.com>
2021-03-20 13:39:06 +0100

    changed paths

--- frontend/src/Menu.js
+++ frontend/src/Menu.js
@@ -2,15 +2,37 @@ import React from 'react'
 import { NavLink } from 'react-router-dom'
 
 export default function Menu() {
-    return(
-        <nav>
-            <ul>
-                <li>
-                    <NavLink to="/receipes/all">
-                        Minden recept
-                    </NavLink>
-                </li>
-            </ul>
-        </nav>
-    )
+  return (
+    <nav>
+      <ul>
+        <li className="nav-item border border-white p-2">
+          <NavLink
+            className="nav-link font-weight-bold"
+            activeClassName="active"
+            to="/all"
+          >
+            Minden recept
+          </NavLink>
+        </li>
+        <li>
+          <NavLink to="/pasta">Tészták</NavLink>
+        </li>
+        <li>
+          <NavLink to="/cookies">Sütemények</NavLink>
+        </li>
+        <li>
+          <NavLink to="/breakfast">Reggelik</NavLink>
+        </li>
+        <li>
+          <NavLink to="/seasonfoods">Szezon ételek</NavLink>
+        </li>
+        <li>
+          <NavLink to="/grill">Grill</NavLink>
+        </li>
+        <li>
+          <NavLink to="/tips">+Tippek</NavLink>
+        </li>
+      </ul>
+    </nav>
+  )
 }
--- frontend/src/Routing.js
+++ frontend/src/Routing.js
@@ -1,21 +1,63 @@
-import React from "react"
-import {Switch, Route, Redirection} from "react-router-dom"
+import React from 'react'
+import { Switch, Route, Redirect } from 'react-router-dom'
 
 export default function Routing() {
-    return(
-        <div>
-            <Switch>
-                <Route path="/reciepes/all">
-                    <AllReceipes/>
-                </Route>
-
-            </Switch>
-        </div>
-    )
+  return (
+    <div className="col-12">
+      <Switch>
+        <Route exact path="/all">
+          <AllReceipes />
+        </Route>
+        <Route exact path="/pasta">
+          <Pasta />
+        </Route>
+        <Route path="/breakfast">
+          <Breakfast />
+        </Route>
+        <Route path="/cookies">
+          <Cookies />
+        </Route>
+        <Route path="/seasonfoods">
+          <SeasonFoods />
+        </Route>
+        <Route path="/grill">
+          <Grill />
+        </Route>
+        <Route path="/tips">
+          <Tips />
+        </Route>
+        <Route path="/">
+          <Redirect to="/all" />
+        </Route>
+      </Switch>
+    </div>
+  )
 }
 
 function AllReceipes() {
-    return(
-        <div>Minden recept</div>
-    )
+  return <div>Minden recept</div>
+}
+
+function Pasta() {
+  return <div>Tészta félék</div>
+}
+
+function Breakfast() {
+  return <div>Reggelik</div>
+}
+
+function Cookies() {
+  return <div>Sütik</div>
+}
+
+function SeasonFoods() {
+  return <div>Szezonételek</div>
+}
+
+function Grill() {
+  return <div>Grill ételek</div>
+}
+
+function Tips() {
+  return <div>Tippek sütéshez, főzéshez</div>
 }
