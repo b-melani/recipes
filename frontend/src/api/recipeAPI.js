dd3c34523f78a7be2fc3c494779251cfcb369f50
Meli <bodzamelani@gmail.com>
2021-03-21 23:08:31 +0100

    changed port

--- frontend/src/api/recipeAPI.js
+++ frontend/src/api/recipeAPI.js
@@ -2,7 +2,8 @@ import axios from 'axios'
 import { useEffect, useState } from 'react'
 
 async function getPasta() {
-  const { data } = await axios.get('http://localhost:3000/pasta')
+  console.log('Itt hivodik meg az api')
+  const { data } = await axios.get('http://localhost:3001/pasta')
   console.log('APIHIVÁS:')
   console.log(data)
   return data
