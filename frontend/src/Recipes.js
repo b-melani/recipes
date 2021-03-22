845dbd9fe3b01c465393c902e489cfd62e7bd174
Meli <bodzamelani@gmail.com>
2021-03-22 21:14:33 +0100

    impoerted OneRecipe

--- frontend/src/Recipes.js
+++ frontend/src/Recipes.js
@@ -1,9 +1,28 @@
 import React from 'react'
 import usePasta from './hooks/usePasta'
+import OneRecipe from './OneRecipe'
 
 export default function Recipes() {
   const { pasta } = usePasta()
+  console.log(pasta)
 
-  console.log('receptek:' + pasta)
-  return <div>Szia</div>
+  return (
+    <>
+      {pasta ? (
+        <div>
+          {pasta.map((oneItem) => (
+            // <div>
+            //   <div className="d-flex">
+            //     <div>
+            <OneRecipe text={oneItem.name} image={oneItem.img} />
+            //     </div>
+            //   </div>
+            // </div>
+          ))}
+        </div>
+      ) : (
+        <div>'nkutya'</div>
+      )}
+    </>
+  )
 }
