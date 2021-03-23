687571d39f4515f9d3275e4c8286043040373925
Meli <bodzamelani@gmail.com>
2021-03-23 22:43:13 +0100

    changed recipeAPI

--- frontend/src/api/recipeAPI.js
+++ frontend/src/api/recipeAPI.js
@@ -1,12 +1,12 @@
 import axios from 'axios'
 import { useEffect, useState } from 'react'
 
-async function getPasta() {
+async function getRecipes(category) {
   console.log('Itt hivodik meg az api')
-  const { data } = await axios.get('http://localhost:3001/pasta')
+  const { data } = await axios.get(`http://localhost:3001/recipe/${category}`)
   console.log('APIHIVÁS:')
   console.log(data)
   return data
 }
 
-export { getPasta }
+export { getRecipes }
