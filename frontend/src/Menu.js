05724b3a6771aabe57f54fcda74f2feb9bf8c0e2
Meli <bodzamelani@gmail.com>
2021-03-23 22:43:54 +0100

    added onClick to navbars items

--- frontend/src/Menu.js
+++ frontend/src/Menu.js
@@ -1,7 +1,10 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
+import useRecipe from './hooks/useRecipe'
 
 export default function Menu() {
+  const { setCategory } = useRecipe()
+
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-light ">
       <ul className="navbar-nav mr-auto p-0 d-flex justify-content-around w-100 menu">
@@ -10,6 +13,7 @@ export default function Menu() {
             className="nav-link font-weight-bold"
             activeClassName="active"
             to="/all"
+            onClick={() => setCategory('')}
           >
             Minden recept
           </NavLink>
@@ -19,6 +23,7 @@ export default function Menu() {
             className="nav-link font-weight-bold"
             activeClassName="active"
             to="/pasta"
+            onClick={() => setCategory('tészta')}
           >
             Tészták
           </NavLink>
@@ -28,6 +33,7 @@ export default function Menu() {
             className="nav-link font-weight-bold"
             activeClassName="active"
             to="/cookies"
+            onClick={() => setCategory('sütemény')}
           >
             Sütemények
           </NavLink>
@@ -37,6 +43,7 @@ export default function Menu() {
             className="nav-link font-weight-bold"
             activeClassName="active"
             to="/breakfast"
+            onClick={() => setCategory('reggeli')}
           >
             Reggelik
           </NavLink>
@@ -49,20 +56,37 @@ export default function Menu() {
             data-toggle="dropdown"
             activeClassName="active"
             to="/seasonfoods"
+            onClick={() => setCategory('')}
           >
             Szezon ételek
           </NavLink>
           <div className="dropdown-menu" aria-labelledby="dropdownMenuLink">
-            <NavLink to="/seasonfoods/springfoods" className="dropdown-item">
+            <NavLink
+              to="/seasonfoods/springfoods"
+              className="dropdown-item"
+              onClick={() => setCategory('tavasz')}
+            >
               Tavaszi ételek
             </NavLink>
-            <NavLink to="/seasonfoods/summerfoods" className="dropdown-item">
+            <NavLink
+              to="/seasonfoods/summerfoods"
+              className="dropdown-item"
+              onClick={() => setCategory('nyár')}
+            >
               Nyári ételek
             </NavLink>
-            <NavLink to="/seasonfoods/autumnfoods" className="dropdown-item">
+            <NavLink
+              to="/seasonfoods/autumnfoods"
+              className="dropdown-item"
+              onClick={() => setCategory('ősz')}
+            >
               Őszi ételek
             </NavLink>
-            <NavLink to="/seasonfoods/winterfoods" className="dropdown-item">
+            <NavLink
+              to="/seasonfoods/winterfoods"
+              className="dropdown-item"
+              onClick={() => setCategory('tél')}
+            >
               Téli ételek
             </NavLink>
           </div>
@@ -72,6 +96,7 @@ export default function Menu() {
             className="nav-link font-weight-bold"
             activeClassName="active"
             to="/grill"
+            onClick={() => setCategory('grill')}
           >
             Grill
           </NavLink>
@@ -81,6 +106,7 @@ export default function Menu() {
             className="nav-link font-weight-bold"
             activeClassName="active"
             to="/tips"
+            onClick={() => setCategory('tipp')}
           >
             +Tippek
           </NavLink>
