0733fc05a5047587c37a3938638f18cfa9d527e9
Meli <bodzamelani@gmail.com>
2021-03-23 22:44:33 +0100

    changed OneRecipe

--- frontend/src/OneRecipe.js
+++ frontend/src/OneRecipe.js
@@ -2,14 +2,10 @@ import React from 'react'
 
 export default function OneRecipe({ text, image }) {
   console.log('képcimee')
-  console.log(image)
+  //console.log(image)
   return (
     <div className="card" styles="width: 18rem;">
-      <img
-        src={require('../backend/images/cheesecake')}
-        className="card-img-top"
-        alt="img"
-      />
+      <img src={image} className="card-img-top" alt="img" />
       <div className="card-body">
         <p className="card-text">{text}</p>
       </div>
